72ffac3eea280dacec21f5ef8823d242420031f9
GolasalaPuneeth <97512630+GolasalaPuneeth@users.noreply.github.com>
2023-09-05 14:58:19 +0530

    Update main.py
    Updated with source code

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-from flask import Flask, jsonify
-import os
+import io
+from flask import*
+from flask import Flask,render_template,request,redirect,flash
+from werkzeug.utils import secure_filename
 
+import io
+import os
+import webbrowser
+import json
+import time
 app = Flask(__name__)
 
+    
+@app.route('/config',methods=['POST','GET'])
+def config():
+    if request.method == 'POST':
+        ssid = request.form['wifi']
+        pask = request.form['password']
+        confirmpassword=request.form['confirmpassword']
+        
 
-@app.route('/')
-def index():
-    return "Hello Everyone"
+        filename = 'wpa_supplicant.conf'
+        with open(filename, 'a') as file:
+            file.write("country=IN\n")
+            file.write("ctrl_interface=DIR=/var/run/wpa_supplicant\n")
+            file.write("GROUP=netdev\n")
+            file.write("update_config=1\n")
+            file.write("network={\n")
+            file.write(f"""\tssid="{ssid}"\n""")
+            file.write(f"""\tpsk="{pask}"\n""")
+            file.write("}\n")
+            
+
+        return send_file(filename, as_attachment=True)
+
+    return render_template('wifi_up.html') 
 
 
 if __name__ == '__main__':
